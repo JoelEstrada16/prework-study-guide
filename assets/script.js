f1973af4358325c139b2aa0b3604c1a3bb185551
Joel Estrada <joeljazzhands@Joels-MacBook-Pro.local>
2023-01-30 11:01:11 -0800

    Changed hpw the console looks using JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,27 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics(){
+    for (var x=0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
-/*
-if (topic === 'HTML') {
+
+function selectTopic() {
+if (randomTopic === 'HTML') {
     console.log("lets study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
     console.log("lets study CSS");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
     console.log("lets study Git");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
     console.log("lets study JavaScript");
 } else {
     console.log('Please try again');
 }
-*/
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
